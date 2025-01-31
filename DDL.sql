7855c81409437e3fec1d14e1177bdfd4c3468fa2
jmbosley <38381775+jmbosley@users.noreply.github.com>
2025-01-31 15:57:43 -0700

    Update DDL.sql
    Added in Intermediate Tables

--- DDL.sql
+++ DDL.sql
@@ -3,6 +3,7 @@
 --Team Name: Team 143
 
 -- Data Definition Queries
+-- Entity Tables
 CREATE OR REPLACE TABLE Artists 
 	(
         artistID int AUTO_INCREMENT,
@@ -65,6 +66,61 @@ CREATE OR REPLACE TABLE Reviews
         -- The combination of the first_name and last_name must be unique in this table. Name this constraint as full_name 
         PRIMARY KEY (reviewID)
         FOREIGN KEY (customerID) REFERENCES Customers(customerID),
+);
+
+-- Intermediate Tables
+CREATE OR REPLACE TABLE ArtistGenres(
+	artistGenreID INT,
+	artistID INT,
+	genreID INT,	
+	FOREIGN KEY (artistID) REFERENCES Artists(artistID),
+	FOREIGN KEY (genreID) REFERENCES Genres(genreID),
+	PRIMARY KEY (artistGenreID) REFERENCES(artistID, genreID)
+);
+
+CREATE OR REPLACE TABLE ArtistMediums(
+	artistMediumID INT,
+	artistID INT,
+	mediumID INT,	
+	FOREIGN KEY (artistID) REFERENCES Artists(artistID),
+	FOREIGN KEY (mediumID) REFERENCES Mediums(mediumID),
+	PRIMARY KEY (artistMediumID) REFERENCES(artistID, mediumID)
+);
+
+CREATE OR REPLACE TABLE CommissionMediums(
+	commissionMediumID INT,
+	commissionID INT,
+	mediumID INT,	
+	FOREIGN KEY (commissionID) REFERENCES Commissions(commissionID),
+	FOREIGN KEY (mediumID) REFERENCES Mediums(mediumID),
+	PRIMARY KEY (commissionMediumID) REFERENCES(commissionID, mediumID)
+);
+
+CREATE OR REPLACE TABLE CommissionGenres(
+	commissionMediumID INT,
+	commissionID INT,
+	genreID INT,	
+	FOREIGN KEY (commissionID) REFERENCES Commissions(commissionID),
+	FOREIGN KEY (genreID) REFERENCES Genres(genreID),
+	PRIMARY KEY (commissionMediumID) REFERENCES(commissionID, genreID)
+);
+
+CREATE OR REPLACE TABLE ArtistComissions(
+	artistCommissionID INT,
+	artistID INT,
+	commissionID INT,
+	FOREIGN KEY (commissionID) REFERENCES Commissions(commissionID),
+	FOREIGN KEY (artistID) REFERENCES Artists(artistID),
+	PRIMARY KEY (artistCommissionID) REFERENCES(commissionID, artistID)
+);
+
+CREATE OR REPLACE TABLE ArtistReviews(
+	artistReviewsID INT,
+	artistID INT,
+	reviewID INT,
+	FOREIGN KEY (reviewID) REFERENCES Reviews(reviewID),
+	FOREIGN KEY (artistID) REFERENCES Artists(artistID),
+	PRIMARY KEY (artistReviewsID) REFERENCES(commissionID, artistID)
 );
 
 
