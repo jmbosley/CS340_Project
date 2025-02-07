32f422c2c47f61e5bd7a830a9dfe9490a5d2fbb4
Carolina-Wren <146513548+Carolina-Wren@users.noreply.github.com>
2025-02-06 19:48:47 -0500

    Removed rreviews Added DATETIME to commissions dateRequested Added more data

--- DDL.sql
+++ DDL.sql
@@ -12,8 +12,7 @@ CREATE OR REPLACE TABLE Artists
 	(
         artistID int AUTO_INCREMENT,
         email varchar(145) NOT NULL,
-	name varchar(145) NOT NULL,
-        averageRating DECIMAL(2,1),
+		name varchar(145) NOT NULL,
         completedCount INT NOT NULL,
         -- Unique Email
         PRIMARY KEY (artistID),
@@ -49,26 +48,13 @@ CREATE OR REPLACE TABLE Commissions
 	(
         commissionID int AUTO_INCREMENT,
         requestStatus VARCHAR(145) NOT NULL,
-        dateRequested DATE NOT NULL,
+        dateRequested DATETIME NOT NULL, -- format: 1000-01-01 00:00:00
         dateCompleted DATE,
         price DECIMAL(9,2) NOT NULL,
         customerID INT,
         PRIMARY KEY (commissionID),
         FOREIGN KEY (customerID) REFERENCES Customers(customerID)
 			ON DELETE CASCADE -- if the foreign key is deleted it will be deleted here too
-		-- CONSTRAINT onePerDay UNIQUE (customerID, dateRequested)
-);
-
-CREATE OR REPLACE TABLE Reviews 
-	(
-        reviewID int AUTO_INCREMENT,
-        starValue INT NOT NULL,
-        reviewText VARCHAR(250),
-        reviewDate DATE NOT NULL,
-        customerID INT,
-        PRIMARY KEY (reviewID),
-        FOREIGN KEY (customerID) REFERENCES Customers(customerID)
-			ON DELETE CASCADE
 );
 
 -- Intersection Tables
@@ -127,24 +113,14 @@ CREATE OR REPLACE TABLE ArtistCommissions(
 	PRIMARY KEY (artistCommissionID)
 );
 
-CREATE OR REPLACE TABLE ArtistReviews(
-	artistReviewID INT NOT NULL AUTO_INCREMENT,
-	artistID INT,
-	reviewID INT,
-	FOREIGN KEY (reviewID) REFERENCES Reviews(reviewID)
-		ON DELETE CASCADE,
-	FOREIGN KEY (artistID) REFERENCES Artists(artistID)
-		ON DELETE CASCADE,
-	PRIMARY KEY (artistReviewID)
-);
-
 
 -- Sample Data INSERT statements
 -- Entity Tables
 -- Insert Into Artists
-INSERT INTO Artists (email, name, averageRating, completedCount)
-	VALUES('JaneDoe@gmail.com', 'Jane Doe', 3.0, 1), ('ghostShipGames@gmail.com', 'Peter Smith', 5.0, 1), ('watercolorlover@gmail.com', 'Betty Davis', 1.0, 1),
-	('lazyArtist@gmail.com', 'Jenny Taylor', NULL, 0);
+
+INSERT INTO Artists (email, name, completedCount)
+	VALUES('JaneDoe@gmail.com', 'Jane Doe', 1), ('ghostShipGames@gmail.com', 'Peter Smith', 1), ('watercolorlover@gmail.com', 'Betty Davis', 2),
+	('lazyArtist@gmail.com', 'Jenny Taylor', 0);
 
 -- Insert Into Customers
 INSERT INTO Customers (email, name, birthday, totalOrders)
@@ -160,65 +136,67 @@ INSERT INTO Mediums(type)
 
 -- Insert into Commissions
 INSERT INTO Commissions(requestStatus, dateRequested, dateCompleted, price, customerID)
-	VALUES('Request Complete', '2017-07-22', '2017-08-02', 50.00, (SELECT customerID from Customers WHERE email='JoeShmow@yahoo.com')),
-	('Request Complete', '2020-01-16', '2020-11-19', 12.32, (SELECT customerID from Customers WHERE email='wWilson@hotmail.com')),
-	('Request Complete', '2021-10-02', '2021-11-05', 20.00, (SELECT customerID from Customers WHERE email='bobTheArtLover@gmail.com')),
-	('Request Unclaimed', '2021-12-20', NULL, 95.50, (SELECT customerID from Customers WHERE email='bobTheArtLover@gmail.com'));
-
--- Insert into Reviews
-INSERT INTO Reviews(starValue, reviewText, reviewDate, customerID)
-	VALUES(3, NULL, '2017-08-02', (SELECT customerID from Customers WHERE email='JoeShmow@yahoo.com')),
-	(5,'This artist was great to work with', '2020-11-19', (SELECT customerID from Customers WHERE email='wWilson@hotmail.com')),
-	(1,'This is so ugly!!', '2021-11-05', (SELECT customerID from Customers WHERE email='bobTheArtLover@gmail.com'));
+	VALUES('Request Complete', '2017-07-22 02:10:02', '2017-08-02', 50.00, (SELECT customerID from Customers WHERE email='JoeShmow@yahoo.com')),
+	('Request Complete', '2018-02-02 12:01:05', '2018-03-20', 75.00, (SELECT customerID from Customers WHERE email='JoeShmow@yahoo.com')), -- new joe, betty
+	('Request Complete', '2020-01-16 10:22:32', '2020-11-19', 12.32, (SELECT customerID from Customers WHERE email='wWilson@hotmail.com')),
+	('Request Complete', '2021-10-02 07:18:00', '2021-11-05', 20.00, (SELECT customerID from Customers WHERE email='bobTheArtLover@gmail.com')),
+	('Request Unclaimed', '2021-12-20 20:00:02', NULL, 95.50, (SELECT customerID from Customers WHERE email='bobTheArtLover@gmail.com'));
 
 -- Interaction Tables
 -- Insert Into ArtistGenres
 INSERT INTO ArtistGenres(artistID, genreID)
 	VALUES((SELECT artistID from Artists WHERE email='JaneDoe@gmail.com'), (SELECT genreID from Genres WHERE type='Steampunk')),
 	((SELECT artistID from Artists WHERE email='ghostShipGames@gmail.com'), (SELECT genreID from Genres WHERE type='Horror')),
-	((SELECT artistID from Artists WHERE email='watercolorlover@gmail.com'), (SELECT genreID from Genres WHERE type='Cute'));
+	((SELECT artistID from Artists WHERE email='ghostShipGames@gmail.com'), (SELECT genreID from Genres WHERE type='Modern')),
+	((SELECT artistID from Artists WHERE email='watercolorlover@gmail.com'), (SELECT genreID from Genres WHERE type='Cute')),
+	((SELECT artistID from Artists WHERE email='watercolorlover@gmail.com'), (SELECT genreID from Genres WHERE type='Horror'));
 
 -- Insert Into ArtistMediums
 INSERT INTO ArtistMediums(artistID, mediumID)
 	VALUES((SELECT artistID from Artists WHERE email='JaneDoe@gmail.com'), (SELECT mediumID from Mediums WHERE type='Oil Paint')),
 	((SELECT artistID from Artists WHERE email='ghostShipGames@gmail.com'), (SELECT mediumID from Mediums WHERE type='Digital')),
-	((SELECT artistID from Artists WHERE email='watercolorlover@gmail.com'), (SELECT mediumID from Mediums WHERE type='Watercolor'));
+	((SELECT artistID from Artists WHERE email='ghostShipGames@gmail.com'), (SELECT mediumID from Mediums WHERE type='Oil Paint')),
+	((SELECT artistID from Artists WHERE email='watercolorlover@gmail.com'), (SELECT mediumID from Mediums WHERE type='Watercolor')),
+	((SELECT artistID from Artists WHERE email='watercolorlover@gmail.com'), (SELECT mediumID from Mediums WHERE type='Digital'));
 
 -- Insert Into CommissionMediums
 INSERT INTO CommissionMediums(commissionID, mediumID)
-	-- joe's commission
-	VALUES((SELECT commissionID from Commissions WHERE dateRequested='2017-07-22' AND customerID=1), (SELECT mediumID from Mediums WHERE type='Oil Paint')),
-	-- wade's commission
-	((SELECT commissionID from Commissions WHERE dateRequested='2020-01-16' AND customerID=2), (SELECT mediumID from Mediums WHERE type='Digital')),
-	-- bob's commission
-	((SELECT commissionID from Commissions WHERE dateRequested='2021-10-02' AND customerID=3), (SELECT mediumID from Mediums WHERE type='Watercolor')),
-	((SELECT commissionID from Commissions WHERE dateRequested='2021-12-20' AND customerID=3), (SELECT mediumID from Mediums WHERE type='Gouache'));
+	-- joe, jane
+	VALUES((SELECT commissionID from Commissions WHERE dateRequested='2017-07-22 02:10:02' AND customerID=1), (SELECT mediumID from Mediums WHERE type='Oil Paint')),
+	-- joe, betty
+	((SELECT commissionID from Commissions WHERE dateRequested='2018-02-02 12:01:05' AND customerID=1), (SELECT mediumID from Mediums WHERE type='Digital')),
+	-- wade, peter
+	((SELECT commissionID from Commissions WHERE dateRequested='2020-01-16 10:22:32' AND customerID=2), (SELECT mediumID from Mediums WHERE type='Digital')),
+	((SELECT commissionID from Commissions WHERE dateRequested='2020-01-16 10:22:32' AND customerID=2), (SELECT mediumID from Mediums WHERE type='Oil Paint')),
+	-- bob, betty
+	((SELECT commissionID from Commissions WHERE dateRequested='2021-10-02 07:18:00' AND customerID=3), (SELECT mediumID from Mediums WHERE type='Watercolor')),
+	-- bob, unclaimed
+	((SELECT commissionID from Commissions WHERE dateRequested='2021-12-20 20:00:02' AND customerID=3), (SELECT mediumID from Mediums WHERE type='Gouache'));
 
 -- Insert Into CommissionGenres
 INSERT INTO CommissionGenres(commissionID,genreID)
-	-- joe
-	VALUES((SELECT commissionID from Commissions WHERE dateRequested='2017-07-22' AND customerID=1), (SELECT genreID from Genres WHERE type='Steampunk')),
-	((SELECT commissionID from Commissions WHERE dateRequested='2020-01-16' AND customerID=2), (SELECT genreID from Genres WHERE type='Horror')),
-	-- bob
-	((SELECT commissionID from Commissions WHERE dateRequested='2021-10-02' AND customerID=3), (SELECT genreID from Genres WHERE type='Cute')),
-	((SELECT commissionID from Commissions WHERE dateRequested='2021-12-20' AND customerID=3), (SELECT genreID from Genres WHERE type='Modern'));
+	-- joe, jane
+	VALUES((SELECT commissionID from Commissions WHERE dateRequested='2017-07-22 02:10:02' AND customerID=1), (SELECT genreID from Genres WHERE type='Steampunk')),
+	-- joe, betty
+	((SELECT commissionID from Commissions WHERE dateRequested='2018-02-02 12:01:05' AND customerID=1), (SELECT genreID from Genres WHERE type='Horror')),
+	-- wade, peter
+	((SELECT commissionID from Commissions WHERE dateRequested='2020-01-16 10:22:32' AND customerID=2), (SELECT genreID from Genres WHERE type='Horror')),
+	((SELECT commissionID from Commissions WHERE dateRequested='2020-01-16 10:22:32' AND customerID=2), (SELECT genreID from Genres WHERE type='Modern')),
+	-- bob, betty
+	((SELECT commissionID from Commissions WHERE dateRequested='2021-10-02 07:18:00' AND customerID=3), (SELECT genreID from Genres WHERE type='Cute')),
+	-- bob, unclaimed
+	((SELECT commissionID from Commissions WHERE dateRequested='2021-12-20 20:00:02' AND customerID=3), (SELECT genreID from Genres WHERE type='Modern'));
 
 -- Insert Into ArtistComissions
 INSERT INTO ArtistCommissions(artistID,commissionID)
 	-- joe, jane
-	VALUES((SELECT artistID from Artists WHERE email='JaneDoe@gmail.com'), (SELECT commissionID from Commissions WHERE dateRequested='2017-07-22' AND customerID=1)),
+	VALUES((SELECT artistID from Artists WHERE email='JaneDoe@gmail.com'), (SELECT commissionID from Commissions WHERE dateRequested='2017-07-22 02:10:02' AND customerID=1)),
+	-- joe, betty
+	((SELECT artistID from Artists WHERE email='watercolorlover@gmail.com'), (SELECT commissionID from Commissions WHERE dateRequested='2018-02-02 12:01:05' AND customerID=1)),
 	-- wade, peter
-	((SELECT artistID from Artists WHERE email='ghostShipGames@gmail.com'), (SELECT commissionID from Commissions WHERE dateRequested='2020-01-16' AND customerID=2)),
+	((SELECT artistID from Artists WHERE email='ghostShipGames@gmail.com'), (SELECT commissionID from Commissions WHERE dateRequested='2020-01-16 10:22:32' AND customerID=2)),
 	-- bob, betty
-	((SELECT artistID from Artists WHERE email='watercolorlover@gmail.com'), (SELECT commissionID from Commissions WHERE dateRequested='2021-10-02' AND customerID=3)); 
+	((SELECT artistID from Artists WHERE email='watercolorlover@gmail.com'), (SELECT commissionID from Commissions WHERE dateRequested='2021-10-02 07:18:00' AND customerID=3));
 
--- Insert Into ArtistReviews
-INSERT INTO ArtistReviews(artistID, reviewID)
-	-- joe reviews jane
-	VALUES((SELECT artistID from Artists WHERE email='JaneDoe@gmail.com'),(SELECT reviewID from Reviews WHERE customerID=1 AND reviewDate='2017-08-02')),
-	-- wade reviews peter
-	((SELECT artistID from Artists WHERE email='ghostShipGames@gmail.com'),(SELECT reviewID from Reviews WHERE customerID=2 AND reviewDate='2020-11-19')),
-	-- bob reviews betty
-	((SELECT artistID from Artists WHERE email='watercolorlover@gmail.com'),(SELECT reviewID from Reviews WHERE customerID=3 AND reviewDate='2021-11-05')); 
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
