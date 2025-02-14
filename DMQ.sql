cf9167643b7bf7b897f3419fbe489bffc5c7848e
Anna Connelly <connelan@oregonstate.edu>
2025-02-13 19:31:51 -0800

    Insert statements

--- DMQ.sql
+++ DMQ.sql
@@ -17,3 +17,29 @@ INSERT INTO Mediums(type)
 INSERT INTO Commissions(requestStatus, dateRequested, dateCompleted, price, customerID)
 	VALUES(:requestStatusInput, :dateRequestedInput, :dateCompletedInput, :priceInput, :customerIDInput);
 
+-- Interaction Tables
+-- Insert Into ArtistGenres
+INSERT INTO ArtistGenres(artistID, genreID)
+	VALUES((SELECT artistID from Artists WHERE Artists.email=:artistEmailInput), (SELECT genreID from Genres WHERE Genres.type=:genreTypeInput));
+
+-- Insert Into ArtistMediums
+INSERT INTO ArtistMediums(artistID, mediumID)
+	VALUES((SELECT artistID from Artists WHERE Artists.email=:artistEmailInput), (SELECT mediumID from Mediums WHERE Mediums.type=:mediumTypeInput));
+
+-- Insert Into CommissionMediums
+INSERT INTO CommissionMediums(commissionID, mediumID)
+	VALUES((SELECT commissionID from Commissions WHERE Commissions.dateRequested=:dateRequestedInput AND customerID=(SELECT customerID from Customers WHERE Customers.name=:customerNameInput)),
+    (SELECT mediumID from Mediums WHERE Mediums.type=:mediumTypeInput));
+
+-- Insert Into CommissionGenres
+INSERT INTO CommissionGenres(commissionID,genreID)
+	VALUES((SELECT commissionID from Commissions WHERE Commissions.dateRequested=:dateRequestedInput AND customerID=(SELECT customerID from Customers WHERE Customers.name=:customerNameInput)),
+    (SELECT genreID from Genres WHERE Genres.type=:genreTypeInput));
+
+-- Insert Into ArtistComissions
+INSERT INTO ArtistCommissions(artistID,commissionID)
+	VALUES((SELECT artistID from Artists WHERE Artists.email=:artistEmailInput),
+    (SELECT commissionID from Commissions WHERE Commissions.dateRequested=:dateRequestedInput AND customerID=(SELECT customerID from Customers WHERE Customers.name=:customerNameInput));
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
